946a7c62f92dfea0952f954a5186f12f4f3a1300
Ben Follington <bfollington@gmail.com>
2015-06-01 14:33:15 +1000

    Initial Commit

---
+++ c-utils/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  c-utils
+//
+//  Created by Ben Follington on 1/06/2015.
+//  Copyright (c) 2015 Ben Follington. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
